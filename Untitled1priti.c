8a1bd74d00b323822e08d63b4c2e88a26c478038
priti664 <pritibhanuverma2007@gmail.com>
2024-10-11 09:25:55 +0530

    Add files via upload

---
+++ Untitled1priti.c
@@ -0,0 +1,16 @@
+
+
+
+
+
+#include<stdio.h>
+
+int main(){
+     printf("Name\t\t :\t Priti\n");
+    printf("UIN\t\t :\t241P012\n");
+    printf("Age\t\t :\t17\n");
+    printf("Division\t :\tC\n");
+    printf("class\t\t :\tEC\n");
+
+     return 0;
+}
